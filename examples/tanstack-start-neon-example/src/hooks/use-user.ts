83dc2ce3f5235679d5614eba5ffe89f732d0c1f3
daveycodez <daveycodez@gmail.com>
2026-09-09 16:37:58 -0700

    refactor(example): return the users query from useUser as is

--- examples/tanstack-start-neon-example/src/hooks/use-user.ts
+++ examples/tanstack-start-neon-example/src/hooks/use-user.ts
@@ -1,12 +1,7 @@
-import { AuthRequiredError } from "@neondatabase/postgrest-js"
 import { useQuery } from "@supabase-cache-helpers/postgrest-react-query"
 
 import { client } from "../lib/client"
 
 export function useUser() {
-  const { data, error, isPending } = useQuery(
-    client.from("users").select().single()
-  )
-
-  return { data: error instanceof AuthRequiredError ? null : data, isPending }
+  return useQuery(client.from("users").select().single())
 }
